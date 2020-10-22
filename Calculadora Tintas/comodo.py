a16e602a18784293ecebc20afc0982e1ad497f50
marciogarridoLaCop <marciogarrido@id.uff.br>
2020-10-21 22:52:37 -0300

    Propriedades e setters

--- Calculadora Tintas/comodo.py
+++ Calculadora Tintas/comodo.py
@@ -1,9 +1,33 @@
 class Comodo:
-   __largura: float
-   __profundidade: float
-   __altura: float
+    __largura: float
+    __profundidade: float
+    __altura: float
 
     def __init__(self, largura, profundidade):
         self.largura = largura
         self.profundidade = profundidade
-        self.altura = 2.9
+        self.__altura = 2.9
+
+    @property
+    def largura(self):
+        return self.__altura
+
+    @property
+    def profundidade(self):
+        return self.__profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @largura.setter
+    def largura(self,largura):
+        self.__largura = largura
+
+    @profundidade.setter
+    def profundidade(self,profundidade):
+        self.__profundidade = profundidade
+
+    @altura.setter
+    def altura(self,altura):
+        self.__altura = altura
